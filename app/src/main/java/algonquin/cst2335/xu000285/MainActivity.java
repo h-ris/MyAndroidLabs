da02a05aa3c2c544649f01f4615d068bf7627105
Ris Xu <risxu.ca22@gmail.com>
2023-05-12 13:47:28 -0400

    added comments

--- app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    // equals to staic void main(Stirng args[])
+    @Override //this is the starting point
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //loads the screen
         setContentView(R.layout.activity_main);
     }
 }
