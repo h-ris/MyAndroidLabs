dcf91fb1c3c05595e7ff4d2f5d711c41adf8b35d
Ris Xu <risxu.ca22@gmail.com>
2023-05-18 17:20:19 -0400

    Commit 4 Added extra lines

--- app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
+
         //loads the screen
         setContentView(R.layout.activity_main);
     }
