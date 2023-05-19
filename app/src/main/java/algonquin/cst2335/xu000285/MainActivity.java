4ab5b743c3190269614d2a7ad42dab249ba33296
Ris Xu <risxu.ca22@gmail.com>
2023-05-19 13:43:50 -0400

    setOnClickListener

--- app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
@@ -3,6 +3,9 @@ package algonquin.cst2335.xu000285;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
-
         //loads the screen
         setContentView(R.layout.activity_main);
+
+        TextView theText = findViewById(R.id.textview);
+
+        Button myButton = findViewById(R.id.mybutton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // this gets run when you click the button
+                theText.setText("You clicked the button.");
+                myButton.setText("Something new here.");
+            }
+        });
     }
 }
