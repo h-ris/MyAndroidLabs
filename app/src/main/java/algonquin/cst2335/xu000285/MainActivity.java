3aed3d0e25b1e9b6b7672f0e7c8ab7af41c23bf0
Ris Xu <risxu.ca22@gmail.com>
2023-06-08 16:48:10 -0400

    Lab 4 Part 1 finished

--- app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/xu000285/MainActivity.java
@@ -2,42 +2,57 @@ package algonquin.cst2335.xu000285;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.util.Log;
+
+import algonquin.cst2335.xu000285.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
+    //private MainViewModel model;
+
+    private String TAG = "MainActivity";
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ImageView imgView;
-        Switch sw;
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        imgView = findViewById(R.id.flagView);
-        sw = findViewById(R.id.spin_switch1);
+        Log.e(TAG, "In OnCreate()");
 
-        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+        //view binding
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        //instaniate a view model
+        //model = new ViewModelProvider(this).get(MainViewModel.class);
+        variableBinding.emailPrompt.setText(prefs.getString("Email", ""));
+        //event listener
+        variableBinding.loginBtn.setOnClickListener((v)->{
+            Log.e(TAG, "You clicked the button");
 
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
+            //create next page
+            Intent nextPage = new Intent(this, SecondActivity.class);
 
-                imgView.startAnimation(rotate);
-            }
-            else {
-                imgView.clearAnimation();
-            }
+            //get email and pw from user prompt
+            String emailAddress = variableBinding.emailPrompt.getText().toString();
+            String pw = variableBinding.pwPrompt.getText().toString();
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("Email", emailAddress);
+            editor.apply();
 
+//            nextPage.putExtra("Email", emailAddress);
+//            nextPage.putExtra("Password", pw);
+
+            // next page
+            startActivity(nextPage);
         });
+
     }
 }
